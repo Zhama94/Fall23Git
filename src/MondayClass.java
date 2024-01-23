a18f33107c67863391365732fec0d182d9652d33
Zhama <jamishka7ya@gmail.com>
2024-01-23 11:21:40 +0300

    Some info

---
+++ src/MondayClass.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class MondayClass {
+    public static void main(String[] args) {
+        System.out.println("hello Monday");
+    }
+
+}
---
+++ src/VisaPayment/VisaPayment.java
@@ -0,0 +1,9 @@
+package VisaPayment;
+
+public class VisaPayment {
+
+    public void payByVisa (String cardHolder, int cardNum){
+        System.out.println(cardHolder + "" + cardNum);
+
+    }
+}
