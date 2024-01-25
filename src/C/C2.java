42b7bcfd44be62073e283004489e8ba23c2b17c0
Aizhamal <jamishka7ya@gmail.com>
2024-01-25 11:15:20 +0300

    dfe

--- src/C/C2.java
+++ src/C/C2.java
@@ -3,7 +3,7 @@ package C;
 public class C2 {
     public static void main(String[] args) {
         System.out.println("G L");
-        System.out.println("HiZZZ");
-        System.out.println("gg");
+        System.out.println("Hi");
+        System.out.println("bro");
     }
 }
